7a4b5fc92f96b45576c2e88da01fd15c90cf794b
Enrico Mani <manienrico13@gmail.com>
2023-10-26 12:31:13 +0300

    routing definition

--- src/App.js
+++ src/App.js
@@ -3,15 +3,19 @@ import { Routes,Route } from "react-router-dom";
 
 import { About, Contact, Error, Home, Shop } from "./pages/pages";
 
+import { Navbar } from "./components/components"
 function App() {
   return (
     <div>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="shop" element={<Shop />} />
-        <Route path="about" element={<About />} />
-        <Route path="contact" element={<Contact />} />
-        <Route path="*" element={<Error />} />
+      <Routes >
+        <Route path="/" element={<Navbar />}>
+          <Route index /*path="/home"*/ element={<Home />} />
+          <Route path="shop" element={<Shop />} />
+          <Route path="about" element={<About />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="*" element={<Error />} />
+        
+        </Route>
       </Routes>
     </div>
   );
--- src/components/navbar/navbar.component.jsx
+++ src/components/navbar/navbar.component.jsx
@@ -1,7 +1,12 @@
+import { Outlet,NavLink } from 'react-router-dom'
+
 import './navbar.styles.css'
 
 export default function Navbar() {
   return (
-    <div>navbar.component</div>
+    <div>
+    navbar.component
+    <Outlet />
+    </div>
   )
 }
