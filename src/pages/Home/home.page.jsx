5bea6dca4be1e9c4114e5429a49d60ee2bcdfdbe
Enrico Mani <manienrico13@gmail.com>
2023-10-30 14:48:36 +0300

    homepage hero section bg img addition and code structure

--- src/pages/Home/home.page.jsx
+++ src/pages/Home/home.page.jsx
@@ -1,7 +1,16 @@
+import HomeBg from "../../assets/bg.svg"
+
 import './home.styles.css'
 
 export default function Home() {
   return (
-    <div>home.page</div>
+    <div className="home-container">
+    <div className="hero-container">
+      <div className="bg-container">
+        <img src={HomeBg} alt="scandinavian-interior-mockup-wall-decal-background"/>
+      </div>
+      <div className="hero-text-container"></div>
+    </div>
+    </div>
   )
 }
